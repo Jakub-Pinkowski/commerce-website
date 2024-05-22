94550cb7c43c90c85f21a779206e2bd3dff2b20b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-05-22 23:03:33 +0200

    Refactor page component to remove unnecessary heading and link

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	<header>
+		<h1>Commerce website</h1>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	footer {
+	}
+</style>
