a7e44cfe0aa69119454111d95ae52fc673cdcfb5
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-09 22:39:47 +0200

    chore: Update header component with responsive navbar design

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,27 +6,66 @@
 		<div class="flex-1">
 			<a href="/" class="btn btn-ghost text-xl">Home</a>
 		</div>
-		<div class="flex-none gap-2">
-			<div class="form-control">
-				<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-			</div>
+		<div class="flex-none">
+			<button class="btn btn-circle btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+					/>
+				</svg>
+			</button>
 			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
-					<div class="w-10 rounded-full">
-						<img
-							alt="Your profile"
-							src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-						/>
+				<div tabindex="0" role="button" class="btn btn-circle btn-ghost">
+					<div class="indicator">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+							/>
+						</svg>
+						<span class="badge indicator-item badge-sm">8</span>
 					</div>
 				</div>
-				<ul
+				<div
 					tabindex="-1"
-					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+					class="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow"
 				>
-					<li><a href="/" class="justify-between">Profile</a></li>
-					<li><a href="/">Settings</a></li>
-					<li><a href="/">Logout</a></li>
-				</ul>
+					<div class="card-body">
+						<span class="text-lg font-bold">8 Items</span>
+						<span class="text-info">Subtotal: $999</span>
+						<div class="card-actions">
+							<button class="btn btn-primary btn-block">View cart</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
+				<div class="w-10 rounded-full">
+					<a href="/profile">
+						<img
+							alt="Tailwind CSS Navbar component"
+							src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+						/>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
