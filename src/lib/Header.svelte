3427ab1f86516ed0c7369156e59b4e925e2c9bee
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-09 22:31:54 +0200

    chore: Update header component with new navbar design
    The header component has been updated to incorporate a new navbar design. The previous navigation links have been replaced with a responsive navbar that includes a home button, search input, and user dropdown menu. This change improves the overall user experience and aligns with the updated project design.

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,35 @@
 <script>
-	import { goto } from '$app/navigation';
 </script>
 
 <header>
-	<h1>My Website</h1>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/categories">Categories</a></li>
-			<li><a href="/newest">Newest</a></li>
-			<li><a href="/bestsellers">Bestsellers</a></li>
-			<li><a href="/sale">Sale</a></li>
-		</ul>
-	</nav>
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost text-xl">Home</a>
+		</div>
+		<div class="flex-none gap-2">
+			<div class="form-control">
+				<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+			</div>
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
+					<div class="w-10 rounded-full">
+						<img
+							alt="Your profile"
+							src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+						/>
+					</div>
+				</div>
+				<ul
+					tabindex="-1"
+					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+				>
+					<li><a href="/" class="justify-between">Profile</a></li>
+					<li><a href="/">Settings</a></li>
+					<li><a href="/">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </header>
 
 <style lang="scss">
