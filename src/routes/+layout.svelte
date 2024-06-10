97c3b640c5f703e83c465ca7cae66ba294cfc5c7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-10 20:54:43 +0200

    chore: Update header and footer components with responsive navbar design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import Header from '../lib/Header.svelte';
     import Footer from '../lib/Footer.svelte';
+    import MiniCart from '../lib/MiniCart.svelte';
 	import "../app.css";
 </script>
 
 <div class="app">
     <Header />
+    <MiniCart />
 
 	<main>
 		<slot />
