1977408bcd9bd5aac64d3c61b2955e7f2fd3c1b4
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-16 11:08:11 +0200

    refactor: Update CartProductCard styling and remove TrashIcon
    Improve the styling of the CartProductCard component in CartProductCard.svelte and remove the TrashIcon. This change enhances the visual presentation of the cart items and simplifies the UI by replacing the TrashIcon with a RemoveIcon for deleting products from the cart.

--- src/lib/Header/CartProductCard.svelte
+++ src/lib/Header/CartProductCard.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import TrashIcon from './icons/TrashIcon.svelte';
+	import RemoveIcon from './icons/RemoveIcon.svelte';
 	import MinusIcon from './icons/MinusIcon.svelte';
 	import PlusIcon from './icons/PlusIcon.svelte';
 </script>
 
-<div class="flex py-6 relative">
-	<div class="absolute right-2.5 top-5">
-		<TrashIcon />
+<div class="relative flex py-6">
+	<div class="absolute right-4 top-7">
+		<RemoveIcon />
 	</div>
 	<div>
 		<img src="https://picsum.photos/200/250" alt="random product" />
 	</div>
 	<div class="flex w-full flex-col">
 		<div class="flex flex-col px-4">
-			<span class="text-2xl">Product Name</span>
-			<span class="text-xl">Size</span>
+			<span class="pr-8 text-xl">Random product name goes here</span>
+			<span class="py-2 text-lg">XS</span>
 		</div>
 		<div class="flex w-full justify-between p-4">
 			<div class="flex items-center">
