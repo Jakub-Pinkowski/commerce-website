65c50dee25814ceb516458ed425532f9bdc175c0
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-16 10:49:43 +0200

    refactor: Update MiniCart to use CartProductCard component
    Refactor the MiniCart component in MiniCart.svelte to use the CartProductCard component instead of manually rendering the product details. This change improves code organization and reusability by separating the product card logic into a separate component. It also lays the foundation for future enhancements and modifications to the cart functionality.

---
+++ src/lib/Header/CartProductCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import MinusIcon from './icons/MinusIcon.svelte';
+	import PlusIcon from './icons/PlusIcon.svelte';
+</script>
+
+<div class="flex py-6">
+	<div>
+		<img src="https://picsum.photos/150/200" alt="random product" />
+	</div>
+	<div class="flex flex-col">
+		<div class="flex flex-col p-2">
+			<span class="p-2">Product Name</span>
+			<span class="px-2">Size</span>
+			<span class="p-2">$99</span>
+		</div>
+		<div class="flex p-2">
+			<MinusIcon />
+			<span class="px-4">1</span>
+			<PlusIcon />
+		</div>
+	</div>
+</div>
--- src/lib/Header/MiniCart.svelte
+++ src/lib/Header/MiniCart.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import CloseIcon from './icons/CloseIcon.svelte';
-	import MinusIcon from './icons/MinusIcon.svelte';
-	import PlusIcon from './icons/PlusIcon.svelte';
+    import CartProductCard from './CartProductCard.svelte';
 
 	export let toggleCartAndMenu: () => void;
 </script>
@@ -11,25 +10,9 @@
 		<span class=" text-3xl">Cart</span>
 		<CloseIcon {toggleCartAndMenu} />
 	</div>
-	<div class="relative flex flex-col p-6">
-		<div class="flex py-6">
-			<div>
-				<img src="https://picsum.photos/150/200" alt="random product" />
-			</div>
-			<div class="flex flex-col">
-				<div class="flex flex-col p-2">
-					<span class="p-2">Product Name</span>
-					<span class="px-2">Size</span>
-					<span class="p-2">$99</span>
-				</div>
-				<div class="flex p-2">
-					<MinusIcon />
-					<span class="px-4">1</span>
-					<PlusIcon />
-				</div>
-			</div>
-		</div>
-		<div>Item 2</div>
+	<div class="relative flex flex-col overflow-auto p-6">
+        <CartProductCard />
+        <CartProductCard />
 	</div>
 	<div class="mt-auto flex w-full flex-col items-center p-6">
 		<button class="btn btn-neutral mt-8 w-full max-w-xl"> See the Cart </button>
