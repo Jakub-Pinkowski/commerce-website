328587b77cce6d16e069ff48b09c381af08b511f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-23 18:32:15 +0200

    Swiper installed

--- src/routes/categories/new/+page.svelte
+++ src/routes/categories/new/+page.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
+	import { register } from 'swiper/element/bundle';
+
+	register();
+
+	const spaceBetween = 10;
 </script>
 
 <div>
-    <h1>New</h1>
+	<h1>New</h1>
+	<swiper-container
+		slides-per-view={3}
+		space-between={spaceBetween}
+		centered-slides={true}
+		pagination={{
+			hideOnClick: true
+		}}
+	>
+		<swiper-slide>Slide 1</swiper-slide>
+		<swiper-slide>Slide 2</swiper-slide>
+		<swiper-slide>Slide 3</swiper-slide>
+	</swiper-container>
 </div>
