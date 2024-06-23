aab5cfa48230a1172012caa351c9ddc4a221f204
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-23 17:32:11 +0200

    refactor: Update legal pages with improved styling and content

--- src/routes/help/terms_and_conditions/+page.svelte
+++ src/routes/help/terms_and_conditions/+page.svelte
@@ -47,13 +47,13 @@
 		use [Your Website] if you do not agree to take all of the terms and conditions stated on this
 		page. The following terminology applies to these Terms and Conditions, Privacy Statement and
 		Disclaimer Notice and any or all Agreements: "Client", "You" and "Your" refers to you, the
-		person accessing this website and accepting the Company’s terms and conditions. "The Company",
+		person accessing this website and accepting the Company's terms and conditions. "The Company",
 		"Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to
 		both the Client and ourselves, or either the Client or ourselves. All terms refer to the offer,
 		acceptance, and consideration of payment necessary to undertake the process of our assistance to
 		the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or
-		any other means, for the express purpose of meeting the Client’s needs in respect of provision
-		of the Company’s stated services/products, in accordance with and subject to, prevailing law of
+		any other means, for the express purpose of meeting the Client's needs in respect of provision
+		of the Company's stated services/products, in accordance with and subject to, prevailing law of
 		Netherlands. Any use of the above terminology or other words in the singular, plural,
 		capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to
 		same.
