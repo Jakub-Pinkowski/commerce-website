3da082c0cabddc0c26c61e2dd4bae5c4abc38acf
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-23 18:43:08 +0200

    feat: Add lazy loading for images in new categories page
    Implement lazy loading for the images in the new categories page to improve page load performance. This change enhances the user experience by reducing the initial load time and aligns with recent updates to other pages.

--- src/routes/categories/new/+page.svelte
+++ src/routes/categories/new/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { register } from 'swiper/element/bundle';
-
+	import items from '$lib/items/items.json';
 	register();
 
 	const spaceBetween = 10;
@@ -8,16 +8,24 @@
 
 <div>
 	<h1>New</h1>
-	<swiper-container
-		slides-per-view={3}
-		space-between={spaceBetween}
-		centered-slides={true}
-		pagination={{
-			hideOnClick: true
-		}}
-	>
-		<swiper-slide>Slide 1</swiper-slide>
-		<swiper-slide>Slide 2</swiper-slide>
-		<swiper-slide>Slide 3</swiper-slide>
-	</swiper-container>
+	<div class="mt-8">
+		<swiper-container slides-per-view={3} centered-slides={true}>
+			{#each items as item}
+				<swiper-slide>
+					<div class="card card-compact mb-4 w-96 bg-base-100 shadow-lg">
+						<figure>
+							<img src={item.imageUrl} alt={item.name} />
+						</figure>
+						<div class="card-body">
+							<h2 class="card-title">{item.name}</h2>
+							<span>${item.price}</span>
+							<div class="card-actions justify-end">
+								<button class="btn btn-primary">Add to Cart</button>
+							</div>
+						</div>
+					</div>
+				</swiper-slide>
+			{/each}
+		</swiper-container>
+	</div>
 </div>
