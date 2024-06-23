72103800aa55249c0f33854c5c37ca2a533d491b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-23 12:40:44 +0200

    refactor: Remove MiniCart component from layout.svelte
    Remove the MiniCart component from the layout.svelte file as it is no longer needed. This change simplifies the layout and improves performance by reducing unnecessary rendering of the MiniCart component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script>
 	import Header from '../lib/Header/Header.svelte';
 	import Footer from '../lib/Footer.svelte';
-	import MiniCart from '../lib/MiniCart.svelte';
 	import '../app.css';
 </script>
 
 <div class="app">
 	<Header />
-	<MiniCart />
 
 	<main class="p-6 md:p-8">
 		<slot />
