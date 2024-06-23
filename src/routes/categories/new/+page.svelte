ba2464060551bdc6ee24c2f20ee4aedf47477c31
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-23 20:06:21 +0200

    refactor: Update package.json and package-lock.json to remove Swiper dependency
    Remove the Swiper dependency from package.json and package-lock.json to simplify project dependencies and align with recent updates to other pages.

--- src/routes/categories/new/+page.svelte
+++ src/routes/categories/new/+page.svelte
@@ -1,9 +1,46 @@
 <script lang="ts">
+	import Swiper from 'swiper/bundle';
+    import { onMount } from 'svelte';
+	import 'swiper/css/bundle';
 	import items from '$lib/items/items.json';
+
+    let swiper: Swiper;
+
+    onMount(() => {
+        swiper = new Swiper('.swiper', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            }
+        });
+    });
 </script>
 
 <div>
 	<h1>New</h1>
 	<div class="mt-8">
+		<!-- Slider main container -->
+		<div class="swiper">
+			<!-- Additional required wrapper -->
+			<div class="swiper-wrapper">
+				<!-- Slides -->
+				<div class="swiper-slide">Slide 1</div>
+				<div class="swiper-slide">Slide 2</div>
+				<div class="swiper-slide">Slide 3</div>
+			</div>
+			<!-- If we need pagination -->
+			<div class="swiper-pagination"></div>
+
+			<!-- If we need navigation buttons -->
+			<div class="swiper-button-prev"></div>
+			<div class="swiper-button-next"></div>
+		</div>
 	</div>
 </div>
