566e647ef88d091eef7d3c14fbf8eca58ce50cad
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-27 22:13:24 +0200

    refactor: Update new categories page to use Swiper with 4 slides per view
    Update the new categories page to use Swiper with 4 slides per view instead of 1. This change improves the user experience by displaying more items at once and aligns with recent updates to other pages.

--- src/routes/categories/new/+page.svelte
+++ src/routes/categories/new/+page.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
 	import Swiper from 'swiper/bundle';
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import 'swiper/css/bundle';
 	import items from '$lib/items/items.json';
 
-    let swiper: Swiper;
+	let swiper: Swiper;
 
-    onMount(() => {
-        swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            }
-        });
-    });
+	onMount(() => {
+		swiper = new Swiper('.swiper', {
+			slidesPerView: 4,
+			spaceBetween: 10,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			}
+		});
+	});
 </script>
 
 <div>
@@ -31,9 +26,27 @@
 			<!-- Additional required wrapper -->
 			<div class="swiper-wrapper">
 				<!-- Slides -->
-				<div class="swiper-slide">Slide 1</div>
-				<div class="swiper-slide">Slide 2</div>
-				<div class="swiper-slide">Slide 3</div>
+				{#each items as item}
+					<div class="swiper-slide">
+						<div class="bg-white p-4">
+							<img
+								src={item.imageUrl}
+								alt={item.name}
+								class="h-48 w-full rounded-lg object-cover"
+							/>
+							<div class="mt-4">
+								<span>{item.brand} </span>
+								<h2 class="text-lg font-semibold text-gray-900">{item.name}</h2>
+								{#if item.price < item.listPrice}
+									<span class="text-gray-500 line-through">${item.listPrice}</span>
+									<span class="text-red-500">${item.price}</span>
+								{:else}
+									<span class="mt-2 text-gray-600">Price: ${item.price}</span>
+								{/if}
+							</div>
+						</div>
+					</div>
+				{/each}
 			</div>
 			<!-- If we need pagination -->
 			<div class="swiper-pagination"></div>
