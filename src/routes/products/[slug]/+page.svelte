c96dd41b901d3719ca0b19206c734430079b6f0c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-30 20:55:33 +0200

    refactor: Update product page to include product image and details
    This commit refactors the product page to include the product image and details. It adds two sliders, one for thumbnails and one for the main image, allowing users to view different images of the product. The product name, description, and price are also displayed in a separate section. This change enhances the user experience by providing a more comprehensive view of the product.

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -6,8 +6,17 @@
 </script>
 
 {#if product}
-	<h1>{product.name}</h1>
-	<div>{@html product.category}</div>
+	<section class="flex">
+		<!-- TODO: 2 sliders go here, one for thumbnails, one for main image -->
+		<div class="w-3/5 max-w-[55%] flex-none">
+			<img src={product.imageUrl} alt={product.name} class="w-full" />
+		</div>
+		<div class="w-2/5 max-w-[45%] flex-none p-8">
+			<h1 class="text-xl font-bold">{product.name}</h1>
+			<p class="mt-2">{product.description}</p>
+			<p class="mt-4 font-semibold">{product.price}</p>
+		</div>
+	</section>
 {:else}
-	<h1>No product found</h1>
+	<h1 class="text-xl font-bold">No product found</h1>
 {/if}
