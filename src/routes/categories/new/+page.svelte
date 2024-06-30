8c5a166c9344abed6341fa400c891d53f809678f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-30 16:38:44 +0200

    refactor: Update product data source to use new JSON file
    Update the data source for the products in the Categories page to use the new "products.json" file instead of the previous "items.json" file. This change ensures that the Categories page displays the most up-to-date product information and aligns with recent updates to the data structure.

--- src/routes/categories/new/+page.svelte
+++ src/routes/categories/new/+page.svelte
@@ -2,7 +2,7 @@
 	import Swiper from 'swiper/bundle';
 	import { onMount } from 'svelte';
 	import 'swiper/css/bundle';
-	import items from '$lib/items/items.json';
+	import products from '$lib/products/products.json';
 
 	let swiper: Swiper;
 
@@ -23,22 +23,22 @@
 	<div class="mt-8">
 		<div class="swiper">
 			<div class="swiper-wrapper">
-				{#each items as item}
+				{#each products as product}
 					<div class="swiper-slide">
 						<div class="bg-white p-4">
 							<img
-								src={item.imageUrl}
-								alt={item.name}
+								src={product.imageUrl}
+								alt={product.name}
 								class=" w-full rounded-lg object-cover"
 							/>
 							<div class="mt-4">
-								<span>{item.brand} </span>
-								<h2 class="text-lg font-semibold text-gray-900">{item.name}</h2>
-								{#if item.price < item.listPrice}
-									<span class="text-gray-500 line-through">${item.listPrice}</span>
-									<span class="text-red-500">${item.price}</span>
+								<span>{product.brand} </span>
+								<h2 class="text-lg font-semibold text-gray-900">{product.name}</h2>
+								{#if product.price < product.listPrice}
+									<span class="text-gray-500 line-through">${product.listPrice}</span>
+									<span class="text-red-500">${product.price}</span>
 								{:else}
-									<span class="mt-2 text-gray-600">Price: ${item.price}</span>
+									<span class="mt-2 text-gray-600">Price: ${product.price}</span>
 								{/if}
 							</div>
 						</div>
