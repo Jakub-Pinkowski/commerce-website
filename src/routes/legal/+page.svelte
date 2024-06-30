c12f70ef87c57a435d4ca007990ffe6aaf2932dc
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-30 15:58:10 +0200

    refactor: Update legal pages breadcrumbs
    Update the breadcrumbs in the privacy policy and terms and conditions pages to use the new "/legal" route instead of the previous "/help" route. This change ensures consistent navigation across the website and aligns with recent updates to other pages.

---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<div class="breadcrumbs mb-6 text-sm">
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li>Legal</li>
+	</ul>
+</div>
+
+<div class="container mx-auto px-4">
+	<h1 class="mb-4 mt-8 text-3xl font-bold">Legal Information</h1>
+	<p class="mb-4">
+		Find detailed information about the legal aspects of using our service, including terms and
+		conditions, privacy policy, and more.
+	</p>
+	<div class="mt-8">
+		<h2 class="mb-2 text-xl font-semibold">Quick Links</h2>
+		<ul class="list-disc pl-8">
+			<li class="mb-2">
+				<a href="/legal/terms_and_conditions" class="link">Terms and Conditions</a> - Understand the
+				terms you agree to by using our service.
+			</li>
+			<li class="mb-2">
+				<a href="/legal/privacy_policy" class="link">Privacy Policy</a> - Learn how we handle your data
+				and protect your privacy.
+			</li>
+		</ul>
+	</div>
+	<div>
+		<h3 class="mb-4 mt-8 text-2xl font-semibold">Need More Information?</h3>
+		<p>
+			If you need more detailed information or have specific legal questions, please don't hesitate
+			to get in touch with us directly via our <a href="/company/contact" class="link">Contact Us</a
+			> page.
+		</p>
+	</div>
+</div>
