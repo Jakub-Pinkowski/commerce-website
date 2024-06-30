6488278cce629bbe831d6713119a0e195f7e0750
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-30 19:08:13 +0200

    refactor: Update product page to display product details

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-    export let product = data.props?.product;
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	export let product = data.props?.product;
 </script>
 
 {#if product}
-    <h1>{product.name}</h1>
-    <div>{@html product.category}</div>
+	<h1>{product.name}</h1>
+	<div>{@html product.category}</div>
 {:else}
-    <h1>No product found</h1>
+	<h1>No product found</h1>
 {/if}
