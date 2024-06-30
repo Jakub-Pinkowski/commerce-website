f83c69c5d538e61dc85fb830383940c83492cbd1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-06-30 21:14:37 +0200

    refactor: Update product page layout and styling
    This commit updates the layout and styling of the product page. It introduces a responsive design using flexbox, allowing the page to adapt to different screen sizes. The product image is now displayed at full width on larger screens, and the product details section is aligned vertically on smaller screens. Additionally, the font sizes and spacing have been adjusted for better readability. This change improves the overall visual appeal and user experience of the product page.

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -6,15 +6,22 @@
 </script>
 
 {#if product}
-	<section class="flex">
+	<section class="flex flex-col md:flex-row">
 		<!-- TODO: 2 sliders go here, one for thumbnails, one for main image -->
-		<div class="w-3/5 max-w-[55%] flex-none">
+		<div class="w-full flex-none md:max-w-[55%]">
 			<img src={product.imageUrl} alt={product.name} class="w-full" />
 		</div>
-		<div class="w-2/5 max-w-[45%] flex-none p-8">
-			<h1 class="text-xl font-bold">{product.name}</h1>
-			<p class="mt-2">{product.description}</p>
-			<p class="mt-4 font-semibold">{product.price}</p>
+		<div class="w-full flex-none p-8 md:max-w-[45%]">
+			<h1 class="text-3xl font-bold">{product.name}</h1>
+			<p class="my-8 text-xl">{product.description}</p>
+			{#if product.price < product.listPrice}
+				<span class="mt-4 text-2xl text-gray-500 line-through">${product.listPrice}</span>
+				<span class="text-2xl text-red-600">${product.price}</span>
+			{:else}
+				<span class="mt-4 text-2xl text-gray-500">${product.price}</span>
+			{/if}
+			<button class="btn btn-primary btn-block mt-8 text-lg"> Add to cart </button>
+			<button class="btn btn-neutral btn-block mt-8 text-lg"> Add to wishlist </button>
 		</div>
 	</section>
 {:else}
