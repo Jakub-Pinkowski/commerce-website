76a15f4f6cdcd2fea2d64f5e6a6887a265cfd7ec
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-06 16:33:57 +0200

    refactor: Improve image display and handle product updates on CartProductCard component

--- src/lib/Header/CartProductCard.svelte
+++ src/lib/Header/CartProductCard.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import RemoveIcon from './icons/RemoveIcon.svelte';
-	import MinusIcon from './icons/MinusIcon.svelte';
-	import PlusIcon from './icons/PlusIcon.svelte';
 </script>
 
-<div class="relative flex py-6 border-b border-gray-300">
+<div class="relative flex border-b border-gray-300 py-6">
 	<div class="absolute right-0 top-7">
 		<RemoveIcon />
 	</div>
@@ -13,14 +11,18 @@
 	</div>
 	<div class="flex w-full flex-col">
 		<div class="flex flex-col px-4">
-			<span class="pr-8 text-lg lg:text-xl">Random product name goes here</span>
-			<span class="py-2 lg:text-lg">XS</span>
+			<a href="google.com">
+				<h3 class=" text-lg line-clamp-2">Random name</h3>
+			</a>
+			<span class=" mt-2 line-clamp-1 block text-sm">Random brand</span>
 		</div>
-		<div class="flex w-full justify-between pl-2">
+		<div class="mt-auto flex w-full justify-between pl-2">
 			<div class="flex items-center">
-				<MinusIcon />
-				<span class="px-4 text-lg">1</span>
-				<PlusIcon />
+				<div class="join">
+					<button class="btn join-item h-10 min-h-10 text-lg">-</button>
+					<span class="btn join-item h-10 min-h-10 cursor-default text-lg">1</span>
+					<button class="btn join-item h-10 min-h-10 text-lg">+</button>
+				</div>
 			</div>
 			<div class="flex items-center">
 				<span class="p-0 text-lg text-gray-500">$99</span>
--- src/lib/Header/icons/PlusIcon.svelte
+++
@@ -1,7 +0,0 @@
-<button class="btn btn-circle btn-ghost">
-	<svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="24" height="24"
-		><path
-			d="M17,11H13V7a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1v4H7a1,1,0,0,0-1,1H6a1,1,0,0,0,1,1h4v4a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13h4a1,1,0,0,0,1-1h0A1,1,0,0,0,17,11Z"
-		/></svg
-	>
-</button>
