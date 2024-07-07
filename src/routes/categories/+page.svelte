98b507436d4a8f76a6a275c1789d6f2fbca332cd
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-07 19:03:12 +0200

    refactor: Update categories page with improved tab functionality and lazy loading for product images

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -1,66 +1,113 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import type { Product } from '$lib/productTypes.ts';
-	import CategoryProductCard from '$lib/components/CategoryProductCard.svelte';
 	import RecommendationsCarousel from '$lib/components/RecommendationsCarousel.svelte';
 
 	export let data: PageData;
 	export let products: Product[] = data?.products;
 	let productsPerPage = 8;
-	let categories = ['New', 'Sale', 'Best Sellers'];
+	let mainCategories = ['New', 'Sale', 'Best Sellers'];
+    let activeTabMainCategory: string = 'New';
+	let productCategories = ['Shoes', 'Jackets', 'Pants'];
+    let activeTabProductCategory: string = 'Shoes';
+
 	let newProducts = products.filter((product) => product.label === 'new').slice(0, productsPerPage);
 	let saleProducts = products
 		.filter((product) => product.price < product.listPrice)
 		.slice(0, productsPerPage);
-	products = products.slice(0, productsPerPage);
 	// TODO: Introduce real bestSellers later
 	let bestSellerProducts = products.slice(0, productsPerPage);
 
-	let activeTab: string = 'New';
+    let shoesProducts = products.filter((product) => product.category === 'shoes').slice(0, productsPerPage);
+    let jacketsProducts = products.filter((product) => product.category === 'jackets').slice(0, productsPerPage);
+    let pantsProducts = products.filter((product) => product.category === 'pants').slice(0, productsPerPage);
+
 </script>
 
 <div>
 	{#if products}
-		<!-- TODO: Animate the buttons -->
-		<div class="tabs-boxed tabs mb-4 flex w-full justify-between md:w-96" role="tablist">
-			{#each categories as category (category)}
-				<button
-					class="tab flex-1 {activeTab === category ? 'tab-active' : ''}"
-					on:click={() => (activeTab = category)}
-				>
-					{category}
-				</button>
-			{/each}
+		<div class="my-8">
+			<!-- TODO: Animate the buttons -->
+			<div class="tabs-boxed tabs mb-4 flex w-full justify-between md:w-96" role="tablist">
+				{#each mainCategories as category (category)}
+					<button
+						class="tab flex-1 {activeTabMainCategory === category ? 'tab-active' : ''}"
+						on:click={() => (activeTabMainCategory = category)}
+					>
+						{category}
+					</button>
+				{/each}
+			</div>
+
+			<!-- TODO: Possibly animate those as well, using svelte transitions -->
+			{#if activeTabMainCategory === 'New' && newProducts.length}
+				<div class="mb-8">
+					<RecommendationsCarousel products={newProducts} title="New" />
+					<div class="flex justify-center">
+						<a href="/categories/new" class="btn btn-accent"> View all new prodducts </a>
+					</div>
+				</div>
+			{/if}
+
+			{#if activeTabMainCategory === 'Sale' && newProducts.length}
+				<div class="mb-8">
+					<RecommendationsCarousel products={saleProducts} title="Sale" />
+					<div class="flex justify-center">
+						<a href="/categories/sale" class="btn btn-accent"> View all products on sale </a>
+					</div>
+				</div>
+			{/if}
+
+			{#if activeTabMainCategory === 'Best Sellers' && products.length}
+				<div class="mb-8">
+					<RecommendationsCarousel products={bestSellerProducts} title="Best Sellers" />
+					<div class="flex justify-center">
+						<a href="/categories/best-sellers" class="btn btn-accent"> View all best sellers </a>
+					</div>
+				</div>
+			{/if}
 		</div>
-
-		<!-- TODO: Possibly animate those as well, using svelte transitions -->
-		{#if activeTab === 'New' && newProducts.length}
-			<div class="mb-8">
-				<RecommendationsCarousel products={newProducts} title="New" />
-				<div class="flex justify-center">
-					<a href="/categories/new" class="btn btn-accent"> View all new prodducts </a>
-				</div>
+        <div class="my-8">
+            <div class="tabs-boxed tabs mb-4 flex w-full justify-between md:w-96" role="tablist">
+				{#each productCategories as category (category)}
+					<button
+						class="tab flex-1 {activeTabProductCategory === category ? 'tab-active' : ''}"
+						on:click={() => (activeTabProductCategory = category)}
+					>
+						{category}
+					</button>
+				{/each}
 			</div>
-		{/if}
 
-		{#if activeTab === 'Sale' && newProducts.length}
-			<div class="mb-8">
-				<RecommendationsCarousel products={saleProducts} title="Sale" />
-				<div class="flex justify-center">
-					<a href="/categories/sale" class="btn btn-accent"> View all products on sale </a>
-				</div>
-			</div>
-		{/if}
+            {#if activeTabProductCategory === 'Shoes' && shoesProducts.length}
+                <div class="mb-8">
+                    <RecommendationsCarousel products={shoesProducts} title="Shoes" />
+                    <div class="flex justify-center">
+                        <a href="/categories/shoes" class="btn btn-accent"> View all shoes </a>
+                    </div>
+                </div>
+            {/if}
 
-		{#if activeTab === 'Best Sellers' && products.length}
-			<div class="mb-8">
-				<RecommendationsCarousel products={bestSellerProducts} title="Best Sellers" />
-				<div class="flex justify-center">
-					<a href="/categories/best-sellers" class="btn btn-accent"> View all best sellers </a>
-				</div>
-			</div>
-		{/if}
+            {#if activeTabProductCategory === 'Jackets' && jacketsProducts.length}
+                <div class="mb-8">
+                    <RecommendationsCarousel products={jacketsProducts} title="Jackets" />
+                    <div class="flex justify-center">
+                        <a href="/categories/jackets" class="btn btn-accent"> View all jackets </a>
+                    </div>
+                </div>
+            {/if}
 
+            {#if activeTabProductCategory === 'Pants' && pantsProducts.length}
+                <div class="mb-8">
+                    <RecommendationsCarousel products={pantsProducts} title="Pants" />
+                    <div class="flex justify-center">
+                        <a href="/categories/pants" class="btn btn-accent"> View all pants </a>
+                    </div>
+                </div>
+            {/if}
+        
+                    
+        </div>
 	{:else}
 		<p>No products found</p>
 	{/if}
