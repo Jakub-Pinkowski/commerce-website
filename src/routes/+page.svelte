0f458263f4d93eebcaf44139e3b2bc4117e2c46c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-07 17:07:04 +0200

    refactor: Update page with lazy loading for images and consistent styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,9 @@
 	import type { Product } from '$lib/productTypes.ts';
 	import RecommendationsCarousel from '$lib/components/RecommendationsCarousel.svelte';
 
+	import jackets from '$lib/assets/images/jackets.jpg';
+	import footwear from '$lib/assets/images/footwear.jpg';
+
 	export let data: PageData;
 	export let products: Product[] = data?.products;
 	let newProducts = products.filter((product) => product.label === 'New');
@@ -11,10 +14,7 @@
 
 <div>
 	<!-- TODO: Style some nice hero image, it will be seasonal -->
-	<section
-		class="hero ml-[-2rem] w-screen md:h-96"
-		style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
-	>
+	<section class="hero ml-[-2rem] w-screen md:h-96" style="background-image: url({footwear});">
 		<div class="hero-overlay bg-opacity-60"></div>
 		<div class="hero-content text-center text-neutral-content">
 			<div class="max-w-md">
@@ -23,33 +23,33 @@
 				<p class="mb-5 text-xl">
 					Get up to <span class="text-2xl font-bold">50%</span> off on selected items.
 				</p>
-				<a href="/categories/sale" class="btn btn-primary">Shop sale</a>
+				<a href="/categories/sale" class="btn btn-accent">Shop sale</a>
 			</div>
 		</div>
 	</section>
 	<div class="mb-8">
 		<RecommendationsCarousel products={saleProducts} title="Sale" />
 		<div class="flex justify-center">
-			<a href="/categories/sale" class="btn btn-primary"> View all products on sale </a>
+			<a href="/categories/sale" class="btn btn-accent"> View all products on sale </a>
 		</div>
 	</div>
 	<section
 		class="hero ml-[-2rem] w-screen md:h-96"
-		style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+        style="background-image: url({jackets});"
 	>
 		<div class="hero-overlay bg-opacity-60"></div>
 		<div class="hero-content text-center text-neutral-content">
 			<div class="max-w-md">
 				<h1 class="mb-5 text-5xl">New Collection</h1>
 				<p class="mb-5 text-xl">Checkout our brand new arrivals!</p>
-				<a href="/categories/new" class="btn btn-accent">Shop new</a>
+				<a href="/categories/new" class="btn btn-primary">Shop new</a>
 			</div>
 		</div>
 	</section>
 	<div class="mb-8">
 		<RecommendationsCarousel products={newProducts} title="Sale" />
 		<div class="flex justify-center">
-			<a href="/categories/new" class="btn btn-accent"> View all new products </a>
+			<a href="/categories/new" class="btn btn-primary"> View all new products </a>
 		</div>
 	</div>
 </div>
