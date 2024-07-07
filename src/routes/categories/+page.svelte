bd01db3926fd2d295cd8986b8a63db396f6b70e3
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-07 15:20:02 +0200

    refactor: Update categories page with lazy loading for product images and consistent styling

--- src/routes/categories/+page.svelte
+++ src/routes/categories/+page.svelte
@@ -7,24 +7,41 @@
 	export let products: Product[] = data?.products;
 	let newProducts = products.filter((product) => product.label === 'New');
 	let saleProducts = products.filter((product) => product.price < product.listPrice);
+
+	let activeTab: string = 'New';
 </script>
 
 <div>
-	<h1 class="mb-4 text-3xl font-bold">Categories</h1>
-	{#if newProducts}
+	<div class="tabs-boxed tabs mb-4 flex w-96 justify-between" role="tablist">
+		<button
+			class="tab flex-1 {activeTab === 'New' ? 'tab-active' : ''}"
+			on:click={() => (activeTab = 'New')}>New</button
+		>
+		<button
+			class="tab flex-1 {activeTab === 'Sale' ? 'tab-active' : ''}"
+			on:click={() => (activeTab = 'Sale')}>Sale</button
+		>
+		<button
+			class="tab flex-1 {activeTab === 'All' ? 'tab-active' : ''}"
+			on:click={() => (activeTab = 'All')}>Shop All</button
+		>
+	</div>
+
+	{#if activeTab === 'New' && newProducts.length}
 		<RecommendationsCarousel products={newProducts} title="New" />
-		<!-- NOTE: Haven't decided on the color of the button yet -->
 		<div class="flex justify-center">
 			<a href="/categories/new" class="btn btn-primary"> View all new products </a>
 		</div>
 	{/if}
-	{#if saleProducts}
+
+	{#if activeTab === 'Sale' && saleProducts.length}
 		<RecommendationsCarousel products={saleProducts} title="Sale" />
 		<div class="flex justify-center">
 			<a href="/categories/sale" class="btn btn-accent"> View all products on sale </a>
 		</div>
 	{/if}
-	{#if products}
+
+	{#if activeTab === 'All' && products.length}
 		<RecommendationsCarousel {products} title="All products" />
 		<div class="flex justify-center">
 			<a href="/products/" class="btn btn-accent"> View all products </a>
