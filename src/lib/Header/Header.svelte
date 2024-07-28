679764fb06926b0162fa7417893380a96263ce1d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-28 22:31:33 +0200

    refactor: Remove DesktopMenu component and update Header.svelte

--- src/lib/Header/Header.svelte
+++ src/lib/Header/Header.svelte
@@ -3,11 +3,14 @@
 	import { Hamburger } from 'svelte-hamburgers';
 
 	import { isMiniCartOpen, toggleMiniCart } from '$lib/stores/miniCart';
-    
+
 	import MobileMenu from './MobileMenu.svelte';
-	import DesktopMenu from './DesktopMenu.svelte';
 	import MiniCart from './MiniCart.svelte';
 	import SearchIcon from './icons/SearchIcon.svelte';
+	import LanguageIcon from './icons/LanguageIcon.svelte';
+	import WishlistIcon from './icons/WishlistIcon.svelte';
+	import CartIcon from './icons/CartIcon.svelte';
+	import UserIcon from './icons/UserIcon.svelte';
 
 	let open: boolean = false;
 	let productCategories = ['Shoes', 'Jackets', 'Pants'];
@@ -75,7 +78,26 @@
 		<a href="/" class="btn btn-ghost text-xl">Logo</a>
 	</div>
 	<div class="navbar-end">
-		<DesktopMenu {closeMenu} {toggleCartAndMenu} />
+        <!-- TODO: Implement search experience, different for desktop and mobile -->
+		<!-- <label class=" input input-bordered hidden items-center gap-2 lg:flex">
+			<input type="text" class="grow" placeholder="Search" />
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 16 16"
+				fill="currentColor"
+				class="h-8 w-8 opacity-70"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</label> -->
+		<SearchIcon {closeMenu} className="hidden lg:flex" />
+		<UserIcon {closeMenu} className="hidden lg:flex" />
+		<WishlistIcon {closeMenu} />
+		<CartIcon {toggleCartAndMenu} />
 	</div>
 </header>
 <MiniCart {isCartOpen} />
