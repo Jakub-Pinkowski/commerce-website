129700767f6e21df1f3788332ba1f8f57e90f0db
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-07-28 21:24:55 +0200

    refactor: Improve search functionality in search page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+	import type { Product } from '$lib/productTypes.ts';
+
+	export let data: PageData;
+	export let products: Product[] = data?.products;
+
+	let searchQuery = '';
+
+	$: filteredProducts = products?.filter(
+		(product) =>
+			product.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+			product.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+			product.category.toLowerCase().includes(searchQuery.toLowerCase())
+	);
 </script>
 
 <div>
-    <h1>Search</h1>
+	<h1>Search</h1>
+	<input type="text" class="my-6 bg-slate-200" bind:value={searchQuery} />
+
+	{#if filteredProducts && filteredProducts.length > 0}
+		{#each filteredProducts as product}
+			<p>{product.name}</p>
+		{/each}
+	{:else}
+		<p>No products found</p>
+	{/if}
 </div>
