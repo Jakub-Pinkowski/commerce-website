c246d3e9f4156d7d94278e6a7a287f357bbfc0db
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-08-02 14:25:03 +0200

    refactor: Improve search page layout and add search query validation

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -27,13 +27,25 @@
 		</ul>
 	</div>
 	<h1 class="mb-4 text-3xl font-bold">Search</h1>
-	<input type="text" class="my-6 bg-slate-200" bind:value={searchQuery} />
+	<label class="input input-bordered mb-16 flex max-w-xs items-center gap-2">
+		<input type="text" class="grow" placeholder="Enter your search here" bind:value={searchQuery} />
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 16 16"
+			fill="currentColor"
+			class="h-4 w-4 opacity-70"
+		>
+			<path
+				fill-rule="evenodd"
+				d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+				clip-rule="evenodd"
+			/>
+		</svg>
+	</label>
 	<!-- TODO: Add filters to search and categories -->
 
 	{#if !searchQuery}
 		<!-- TODO: Style it -->
-		<div class="mb-12">Enter your search here</div>
-		<RecommendationsCarousel {products} />
 	{:else if filteredProducts && filteredProducts.length > 0}
 		<div class="grid grid-cols-2 gap-6 md:grid-cols-4">
 			{#each filteredProducts as product}
