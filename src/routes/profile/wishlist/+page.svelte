c7d342a21e582d4f47c44365a1040b452bb1510f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-08-05 23:16:10 +0200

    refactor: Update wishlist page layout and add RecommendationsCarousel

--- src/routes/profile/wishlist/+page.svelte
+++ src/routes/profile/wishlist/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
+
 	import type { PageData } from './$types';
 	import { toggleWishlist } from '$lib/stores/wishlist';
 	import type { Product } from '$lib/productTypes.ts';
 	import CategoryProductCard from '$lib/components/CategoryProductCard.svelte';
-	import { onMount } from 'svelte';
+	import RecommendationsCarousel from '$lib/components/RecommendationsCarousel.svelte';
 
 	export let data: PageData;
 	let wishlist: Product[] = [];
@@ -27,15 +29,16 @@
 			<li>Wishlist</li>
 		</ul>
 	</div>
-	<h1>Wishlist</h1>
+	<h1 class="mb-4 text-3xl font-bold">Wishlist</h1>
 	{#if wishlist.length > 0}
 		<div class="grid grid-cols-2 gap-6 md:grid-cols-4">
-            {#each wishlist as product}
-                <CategoryProductCard {product} />
-            {/each}
+			{#each wishlist as product}
+				<CategoryProductCard {product} />
+			{/each}
 		</div>
 	{:else}
 		<p>Your wishlist is empty.</p>
+		<!-- TODO: Add RecommendationsCarousel -->
 	{/if}
 </div>
 
