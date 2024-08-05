ddec2fedca8cdb4f6047d84ccb68e2c8f561e61c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-08-05 22:04:25 +0200

    refactor: Update wishlist store to persist data in localStorage

---
+++ src/lib/stores/wishlist.ts
@@ -0,0 +1,32 @@
+import { writable, derived } from 'svelte/store';
+import type { Product } from '$lib/productTypes.ts';
+
+let initialWishlist: Product[] = [];
+
+if (typeof window !== 'undefined') {
+	const storedWishlist = localStorage.getItem('wishlist');
+	initialWishlist = storedWishlist ? JSON.parse(storedWishlist) : [];
+}
+
+export const wishlist = writable<Product[]>(initialWishlist);
+
+if (typeof window !== 'undefined') {
+	wishlist.subscribe((items) => {
+		localStorage.setItem('wishlist', JSON.stringify(items));
+	});
+}
+
+export const addToWishlist = (item: Product) => {
+	wishlist.update((items) => {
+		if (!items.find((i) => i.id === item.id)) {
+			items.push(item);
+		}
+		return items;
+	});
+};
+
+export const removeFromWishlist = (item: Product) => {
+	wishlist.update((items) => {
+		return items.filter((i) => i.id !== item.id);
+	});
+};
