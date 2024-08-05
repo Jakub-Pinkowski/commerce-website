87c362e1ed157b5d151a263bc48c29e5aebe365e
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-08-05 23:14:42 +0200

    refactor: Add lazy loading for images in wishlist page

--- src/routes/profile/wishlist/+page.svelte
+++ src/routes/profile/wishlist/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import type { PageData } from './$types';
+	import { toggleWishlist } from '$lib/stores/wishlist';
 	import type { Product } from '$lib/productTypes.ts';
+	import CategoryProductCard from '$lib/components/CategoryProductCard.svelte';
 	import { onMount } from 'svelte';
 
 	export let data: PageData;
@@ -10,8 +13,11 @@
 		data.wishlist.subscribe((items: Product[]) => {
 			wishlist = items;
 		});
+		console.log(wishlist);
 	});
-	console.log(wishlist);
+
+	let toastWishlist: boolean = false;
+	let toastMessage: string = '';
 </script>
 
 <div>
@@ -23,12 +29,32 @@
 	</div>
 	<h1>Wishlist</h1>
 	{#if wishlist.length > 0}
-		<ul>
-			{#each wishlist as item}
-				<li>{item.name}</li>
-			{/each}
-		</ul>
+		<div class="grid grid-cols-2 gap-6 md:grid-cols-4">
+            {#each wishlist as product}
+                <CategoryProductCard {product} />
+            {/each}
+		</div>
 	{:else}
 		<p>Your wishlist is empty.</p>
 	{/if}
 </div>
+
+{#if toastWishlist}
+	<div class="toast toast-center toast-top" transition:fade>
+		<div class="alert alert-success">
+			<span> {@html toastMessage}</span>
+		</div>
+	</div>
+{/if}
+
+<style scoped>
+	.alert-success {
+		--alert-bg: var(--light-accent);
+		color: var(--fallback-ac, oklch(var(--ac) / var(--tw-text-opacity)));
+		border-color: var(--light-accent);
+	}
+
+	.toast:where(.toast-top) {
+		top: 50px;
+	}
+</style>
