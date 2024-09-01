aed87099aa90ea60d433a18b5accffa7b9de8f90
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-01 14:03:13 +0200

    refactor: Update error page background image and styles

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import desert from '$lib/assets/images/desert.jpg';
+</script>
+
+<section
+	class="hero mx-[-1.5rem] w-screen md:mx-[-2rem] md:h-96"
+	style="background-image: url({desert});"
+>
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-2xl">
+			<h1 class="mb-8 text-5xl">Error {$page.status}: {$page.error?.message}</h1>
+            {#if $page.status === 404}
+                <p class="mb-8 text-4xl">Looks like this page doesn't exist</p>
+            {:else}
+                <p class="mb-8 text-4xl">An error occurred</p>
+            {/if}
+			<a href="/" class="btn btn-accent">Homepage</a>
+		</div>
+	</div>
+</section>
