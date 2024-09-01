24e41d8f35b6c3c9e912826f499ce5230cc6d9e0
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-01 14:09:53 +0200

    chore: Update email input style in Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -77,7 +77,7 @@
 				<span>Sign up for a newsletter to get 10% off your first purchase</span>
 			</label>
 			<div class="join mt-4">
-				<input type="text" placeholder="username@site.com" class="input join-item input-bordered" />
+				<input type="text" placeholder="username@site.com" class="input join-item input-bordered text-dark" />
 				<button class="btn btn-primary join-item">Subscribe</button>
 			</div>
 			<span class="mt-3 text-xs">
