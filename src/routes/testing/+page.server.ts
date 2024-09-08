f101e29c3dc005b5c66e9ee4dfe5e3dec2348e00
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-08 11:38:13 +0200

    Refactor +page.server.ts to use a connection pool for querying products

--- src/routes/testing/+page.server.ts
+++ src/routes/testing/+page.server.ts
@@ -1,7 +1,9 @@
 import { sql, createPool } from '@vercel/postgres';
+import { POSTGRES_URL } from '$env/static/private';
 
 export async function load() {
-    const result = await sql`SELECT * FROM products`;
+	const pool = createPool({ connectionString: POSTGRES_URL });
+	const result = await pool.sql`SELECT * FROM products`;
 
 	// Extract only the serializable data
 	const products = result.rows;
