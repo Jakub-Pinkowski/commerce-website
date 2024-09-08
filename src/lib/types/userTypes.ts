3e3e97b1151ec1f055d051c9b7987c8470160a21
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-08 16:16:44 +0200

    Refactor import paths for login buttons

--- src/lib/types/userTypes.ts
+++ src/lib/types/userTypes.ts
@@ -3,5 +3,4 @@ export type User = {
 	email: string;
     password: string;
 	createdAt: Date;
-	updatedAt: Date;
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,7 +6,6 @@ import { mapProducts } from '$lib/helpers/sql';
 export const load: PageServerLoad = async () => {
 	const pool = createPool({ connectionString: POSTGRES_URL });
 	const result = await pool.sql`SELECT * FROM products`;
-
 	const products = mapProducts(result);
 
 	return {
