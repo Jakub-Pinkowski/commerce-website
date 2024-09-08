fb4bd09efaf47780f59031a5255fd66c17646dcb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-08 16:43:23 +0200

    Refactor SQL mapping function to use typed product interface and handle invalid result format

--- src/lib/helpers/sql.ts
+++ src/lib/helpers/sql.ts
@@ -1,23 +1,23 @@
 import type { Product } from '$lib/types/productTypes.ts';
 
 interface SQLProductRow {
-	id: string;
-	name: string;
-	handle: string;
-	category: string;
-	brand: string;
-	description: string;
-	price: string;
-	list_price: string;
-	in_stock: boolean;
-	inventory_level: number;
-	review_count: number;
-	review_rating: number;
-	colors: string[];
-	label: string;
-	url: string;
-	imageurl: string;
-	alternate_images: string[];
+	id: number; // integer, not nullable
+	name: string; // character varying, not nullable
+	handle: string; // character varying, not nullable
+	category: string; // character varying, not nullable
+	brand: string; // character varying, not nullable
+	description: string; // text, not nullable
+	price: number; // numeric, not nullable
+	list_price: number; // numeric, not nullable
+	in_stock: boolean; // boolean, not nullable
+	inventory_level: number; // integer, not nullable
+	review_count?: number; // integer, nullable
+	review_rating?: number; // numeric, nullable
+	colors: string; // character varying, not nullable
+	label?: string; // character varying, nullable
+	url: string; // character varying, not nullable
+	imageurl: string; // character varying, not nullable
+	alternate_images: any[]; // jsonb, not nullable
 }
 
 // Map the result of a SQL query to a Product object
