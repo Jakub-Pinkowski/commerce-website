45a5ceb006f44fb3a4fe251b71ff3b6fed513cbe
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-08 16:30:07 +0200

    Refactor miniCart store to use explicit types for isMiniCartOpen and toggleMiniCart functions

--- src/lib/stores/miniCart.ts
+++ src/lib/stores/miniCart.ts
@@ -1,15 +1,15 @@
 import { writable } from 'svelte/store';
 
-export const isMiniCartOpen = writable(false);
+export const isMiniCartOpen = writable<boolean>(false);
 
-export const toggleMiniCart = () => {
+export const toggleMiniCart = (): void => {
 	isMiniCartOpen.update((isOpen) => !isOpen);
 };
 
-export const openMiniCart = () => {
+export const openMiniCart = (): void => {
 	isMiniCartOpen.set(true);
 };
 
-export const closeMiniCart = () => {
+export const closeMiniCart = (): void => {
 	isMiniCartOpen.set(false);
 };
