0628a2b8e5142980de358d101cec986b34e0aed6
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-14 14:26:30 +0200

    Refactor RecommendationsCarousel component to add loading state with skeleton components

--- src/lib/components/Common/RecommendationsCarousel.svelte
+++ src/lib/components/Common/RecommendationsCarousel.svelte
@@ -5,32 +5,41 @@
 
 	import { capitalizeWords } from '$lib/helpers/functions';
 	import type { Product } from '$lib/types/productTypes';
+	import SkeletonRecommendationsCarousel from './SkeletonRecommendationsCarousel.svelte';
 
 	export let products: Product[];
 	export let title: string = 'You may also like';
-    
+
 	const link = title.toLowerCase().replace(/\s+/g, '_');
 
 	let recommendationsSwiper: Swiper;
+	let loading = true;
 
 	onMount(() => {
-		recommendationsSwiper = new Swiper('.swiper.recommendations-carousel', {
-			slidesPerView: 1.3,
-			spaceBetween: 15,
-			mousewheel: {
-				forceToAxis: true
-			},
-			breakpoints: {
-				768: {
-					slidesPerView: 4,
-					navigation: {
-						nextEl: '.swiper.recommendations-carousel .button-next',
-						prevEl: '.swiper.recommendations-carousel  .button-prev',
-						disabledClass: 'swiper-button-disabled'
+		console.log('loading before swiper', loading);
+		if (products && products.length > 0) {
+			setTimeout(() => {
+				loading = false;
+				recommendationsSwiper = new Swiper('.swiper.recommendations-carousel', {
+					slidesPerView: 1.3,
+					spaceBetween: 15,
+					mousewheel: {
+						forceToAxis: true
+					},
+					breakpoints: {
+						768: {
+							slidesPerView: 4,
+							navigation: {
+								nextEl: '.swiper.recommendations-carousel .button-next',
+								prevEl: '.swiper.recommendations-carousel  .button-prev',
+								disabledClass: 'swiper-button-disabled'
+							}
+						}
 					}
-				}
-			}
-		});
+				});
+			}, 0);
+		}
+		console.log('loading after swiper', loading);
 	});
 </script>
 
@@ -41,92 +50,98 @@
 			<a href="/categories/{link}" class="btn btn-accent mb-2"> View all {title} </a>
 		{/if}
 	</div>
-	<div class="swiper recommendations-carousel mt-4">
-		<div class="swiper-wrapper">
-			{#each products as product}
-				<div class="swiper-slide">
-					<a href={product.url} class="relative block w-full">
-						{#if product.alternateImages.length > 0}
-							<div class="group inset-0">
-								<div class="relative h-full w-full">
-									{#if product.price < product.listPrice}
-										<div class="badge badge-lg absolute left-2 top-2 z-10 text-main-red">Sale</div>
-									{:else if product.label && product.label.trim() !== ''}
-										<div class="badge badge-lg absolute left-2 top-2 z-10 text-primary">
-											{capitalizeWords(product.label)}
-										</div>
-									{/if}
-									<img
-										src={product.imageUrl}
-										alt={product.name}
-										class=" inset-0 block h-auto w-full object-cover opacity-100 md:group-hover:opacity-0"
-									/>
-									<img
-										src={product.alternateImages[0]}
-										alt={`Alternate view of ${product.name}`}
-										class="absolute inset-0 hidden h-auto w-full object-cover opacity-0 md:block md:group-hover:opacity-100"
-									/>
+	{#if loading}
+		<SkeletonRecommendationsCarousel />
+	{:else}
+		<div class="swiper recommendations-carousel mt-4">
+			<div class="swiper-wrapper">
+				{#each products as product}
+					<div class="swiper-slide">
+						<a href={product.url} class="relative block w-full">
+							{#if product.alternateImages.length > 0}
+								<div class="group inset-0">
+									<div class="relative h-full w-full">
+										{#if product.price < product.listPrice}
+											<div class="badge badge-lg absolute left-2 top-2 z-10 text-main-red">
+												Sale
+											</div>
+										{:else if product.label && product.label.trim() !== ''}
+											<div class="badge badge-lg absolute left-2 top-2 z-10 text-primary">
+												{capitalizeWords(product.label)}
+											</div>
+										{/if}
+										<img
+											src={product.imageUrl}
+											alt={product.name}
+											class=" inset-0 block h-auto w-full object-cover opacity-100 md:group-hover:opacity-0"
+										/>
+										<img
+											src={product.alternateImages[0]}
+											alt={`Alternate view of ${product.name}`}
+											class="absolute inset-0 hidden h-auto w-full object-cover opacity-0 md:block md:group-hover:opacity-100"
+										/>
+									</div>
 								</div>
-							</div>
-						{:else}
-							<img src={product.imageUrl} alt={product.name} />
-						{/if}
-					</a>
-					<div class="pt-2">
-						<a href={product.url}>
-							<h3 class="line-clamp-1">{product.name}</h3>
-						</a>
-						<span class="line-clamp-1 block text-sm">{product.brand}</span>
-						<div class="mt-4">
-							{#if product.price < product.listPrice}
-								<span class="mr-1 inline-block text-lg text-gray-500 line-through">
-									${product.listPrice}
-								</span>
-								<span class="inline-block text-lg text-main-red">${product.price}</span>
 							{:else}
-								<span class="inline-block text-lg text-gray-500">${product.price}</span>
+								<img src={product.imageUrl} alt={product.name} />
 							{/if}
+						</a>
+						<div class="pt-2">
+							<a href={product.url}>
+								<h3 class="line-clamp-1">{product.name}</h3>
+							</a>
+							<span class="line-clamp-1 block text-sm">{product.brand}</span>
+							<div class="mt-4">
+								{#if product.price < product.listPrice}
+									<span class="mr-1 inline-block text-lg text-gray-500 line-through">
+										${product.listPrice}
+									</span>
+									<span class="inline-block text-lg text-main-red">${product.price}</span>
+								{:else}
+									<span class="inline-block text-lg text-gray-500">${product.price}</span>
+								{/if}
+							</div>
 						</div>
 					</div>
+				{/each}
+				<!-- TODO: Adjust the arrows to be vertically in the middle of the image -->
+			</div>
+			<div class="hidden md:block">
+				<div
+					class="button-prev absolute left-8 top-1/3 z-10 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer"
+				>
+					<button class="btn btn-circle bg-white shadow-xl">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							id="Outline"
+							viewBox="0 0 24 24"
+							width="36"
+							height="36"
+							><path
+								d="M19,11H9l3.29-3.29a1,1,0,0,0,0-1.42,1,1,0,0,0-1.41,0l-4.29,4.3A2,2,0,0,0,6,12H6a2,2,0,0,0,.59,1.4l4.29,4.3a1,1,0,1,0,1.41-1.42L9,13H19a1,1,0,0,0,0-2Z"
+							/></svg
+						>
+					</button>
 				</div>
-			{/each}
-			<!-- TODO: Adjust the arrows to be vertically in the middle of the image -->
-		</div>
-		<div class="hidden md:block">
-			<div
-				class="button-prev absolute left-8 top-1/3 z-10 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer"
-			>
-				<button class="btn btn-circle bg-white shadow-xl">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						id="Outline"
-						viewBox="0 0 24 24"
-						width="36"
-						height="36"
-						><path
-							d="M19,11H9l3.29-3.29a1,1,0,0,0,0-1.42,1,1,0,0,0-1.41,0l-4.29,4.3A2,2,0,0,0,6,12H6a2,2,0,0,0,.59,1.4l4.29,4.3a1,1,0,1,0,1.41-1.42L9,13H19a1,1,0,0,0,0-2Z"
-						/></svg
-					>
-				</button>
-			</div>
-			<div
-				class="button-next absolute right-8 top-1/3 z-10 -translate-y-1/2 translate-x-1/2 transform cursor-pointer"
-			>
-				<button class="btn btn-circle bg-white shadow-xl">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						id="Outline"
-						viewBox="0 0 24 24"
-						width="36"
-						height="36"
-						><path
-							d="M18,12h0a2,2,0,0,0-.59-1.4l-4.29-4.3a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42L15,11H5a1,1,0,0,0,0,2H15l-3.29,3.29a1,1,0,0,0,1.41,1.42l4.29-4.3A2,2,0,0,0,18,12Z"
-						/></svg
-					>
-				</button>
+				<div
+					class="button-next absolute right-8 top-1/3 z-10 -translate-y-1/2 translate-x-1/2 transform cursor-pointer"
+				>
+					<button class="btn btn-circle bg-white shadow-xl">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							id="Outline"
+							viewBox="0 0 24 24"
+							width="36"
+							height="36"
+							><path
+								d="M18,12h0a2,2,0,0,0-.59-1.4l-4.29-4.3a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42L15,11H5a1,1,0,0,0,0,2H15l-3.29,3.29a1,1,0,0,0,1.41,1.42l4.29-4.3A2,2,0,0,0,18,12Z"
+							/></svg
+						>
+					</button>
+				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 </div>
 
 <style>
---
+++ src/lib/components/Common/SkeletonRecommendationsCarousel.svelte
@@ -0,0 +1,42 @@
+<div class="my-8">
+	<div class="flex items-center justify-between">
+		<div class="skeleton h-8 w-20 lg:w-32"></div>
+		<div class="skeleton h-8 w-24 lg:w-40"></div>
+	</div>
+	<div class="mt-4 grid grid-cols-1 gap-4 md:grid-cols-4">
+		<div class="flex gap-2 w-full flex-shrink-0 md:hidden md:w-full">
+			<div class="flex w-10/12 flex-col">
+				<div class="skeleton h-64 w-full"></div>
+				<div class="pt-2">
+					<div class="skeleton h-6 w-3/4"></div>
+					<div class="skeleton mt-2 h-4 w-32"></div>
+					<div class="mt-4">
+						<div class="skeleton h-6 w-20"></div>
+					</div>
+				</div>
+			</div>
+			<div class="flex w-2/12 flex-col">
+				<div class="skeleton h-64 w-full"></div>
+				<div class="pt-2">
+					<div class="skeleton h-6 w-full"></div>
+					<div class="skeleton mt-2 h-4 w-full"></div>
+					<div class="mt-4">
+						<div class="skeleton h-6 w-full"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		{#each Array(4) as _}
+			<div class="hidden flex-col md:flex">
+				<div class="skeleton h-64 w-full"></div>
+				<div class="pt-2">
+					<div class="skeleton h-6 w-3/4"></div>
+					<div class="skeleton mt-2 h-4 w-32"></div>
+					<div class="mt-4">
+						<div class="skeleton h-6 w-20"></div>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
