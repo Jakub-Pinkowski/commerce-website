71b11b870f6df9f54bcaa399a134bf7b5820c564
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-15 20:48:24 +0200

    Refactor product page to display product details and image gallery

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -1,35 +1,28 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import type { Product } from '$lib/types/productTypes';
+    import type { PageData } from './$types';
+    import type { Product } from '$lib/types/productTypes';
 
-	import { fetchProduct } from '$lib/helpers/fetching';
-	import RecommendationsCarousel from '$lib/components/Common/RecommendationsCarousel.svelte';
-	import Breadcrumbs from '$lib/components/Common/Breadcrumbs.svelte';
-	import GallerySwiper from '$lib/components/Product/GallerySwiper.svelte';
-	import ProductDetails from '$lib/components/Product/ProductDetails.svelte';
-	import SkeletonGallerySwiper from '$lib/components/Product/SkeletonGallerySwiper.svelte';
-	import SkeletonProductDetails from '$lib/components/Product/SkeletonProductDetails.svelte';
-	import ProductNotFound from '$lib/components/Product/ProductNotFound.svelte';
+    import RecommendationsCarousel from '$lib/components/Common/RecommendationsCarousel.svelte';
+    import Breadcrumbs from '$lib/components/Common/Breadcrumbs.svelte';
+    import GallerySwiper from '$lib/components/Product/GallerySwiper.svelte';
+    import ProductDetails from '$lib/components/Product/ProductDetails.svelte';
+    import SkeletonGallerySwiper from '$lib/components/Product/SkeletonGallerySwiper.svelte';
+    import SkeletonProductDetails from '$lib/components/Product/SkeletonProductDetails.svelte';
+    import ProductNotFound from '$lib/components/Product/ProductNotFound.svelte';
 
-	export let data: PageData;
-	const productPromise = fetchProduct(data.product as Product, 500); // 500ms delay to simulate network latency
+    export let data: PageData;
 
-	let breadcrumbs = ['Home', 'Products', data.product.name];
+    const product = data.product as Product;
+    let breadcrumbs = ['Home', 'Products', product?.name];
 </script>
 
-{#await productPromise}
-	<section class="flex flex-col md:flex-row">
-		<SkeletonGallerySwiper />
-		<SkeletonProductDetails />
-	</section>
-{:then product}
-	<Breadcrumbs {breadcrumbs} />
-	<section class="flex flex-col md:flex-row">
-		<GallerySwiper {product} />
-		<ProductDetails {product} />
-	</section>
-	<!-- FIXME: Products vs Product issue -->
-	<!-- <RecommendationsCarousel {products} /> -->
-{:catch error}
-	<ProductNotFound />
-{/await}
+{#if product}
+    <Breadcrumbs {breadcrumbs} />
+    <section class="flex flex-col md:flex-row">
+        <GallerySwiper {product} />
+        <ProductDetails {product} />
+    </section>
+    <!-- <RecommendationsCarousel {products} /> -->
+{:else}
+    <ProductNotFound />
+{/if}
