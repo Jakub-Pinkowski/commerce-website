5a757f6046d26ae012c9fa7848f6849ded0156d7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-18 23:51:44 +0200

    Refactor favicon links and inject @vercel/speed-insights in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,23 @@
-<script>
+<script lang="ts">
 	import Header from '../lib/components/Header/Header.svelte';
 	import Footer from '../lib/components/Footer/Footer.svelte';
 	import '../app.css';
 
-    // Disable Vercel Analytics on Safari
-    const isSafari = () => {
+	// Disable Vercel Analytics and Speed Insights on Safari
+	const isSafari = () => {
 		return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-	}
+	};
 
 	if (typeof window !== 'undefined' && !isSafari()) {
 		import('@vercel/analytics').then(({ inject }) => inject());
+		import('@vercel/speed-insights/sveltekit').then(({ injectSpeedInsights }) =>
+			injectSpeedInsights()
+		);
 	}
 </script>
 
 <svelte:head>
-    <!-- TODO: Fix android icons issue -->
+	<!-- TODO: Fix android icons issue -->
 	<title>E-commerce website</title>
 	<meta name="description" content="E-commerce website" />
 	<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
@@ -28,15 +31,15 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
 	<link rel="apple-touch-icon-precomposed" href="/favicons/apple-icon-precomposed.png" />
 	<link rel="apple-touch-icon" href="/favicons/apple-icon.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="36x36" href="/favicons/android-icon-36x36.png" />
-    <link rel="icon" type="image/png" sizes="48x48" href="/favicons/android-icon-48x48.png" />
-    <link rel="icon" type="image/png" sizes="72x72" href="/favicons/android-icon-72x72.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/android-icon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="144x144" href="/favicons/android-icon-144x144.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+	<link rel="icon" type="image/png" sizes="36x36" href="/favicons/android-icon-36x36.png" />
+	<link rel="icon" type="image/png" sizes="48x48" href="/favicons/android-icon-48x48.png" />
+	<link rel="icon" type="image/png" sizes="72x72" href="/favicons/android-icon-72x72.png" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicons/android-icon-96x96.png" />
+	<link rel="icon" type="image/png" sizes="144x144" href="/favicons/android-icon-144x144.png" />
+	<link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
 	<link rel="manifest" href="/favicons/manifest.json" />
 	<meta name="msapplication-TileColor" content="#ffffff" />
 	<meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
