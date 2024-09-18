e471d0923223311b6196306942cbe3b177b28dfe
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-18 23:26:18 +0200

    Refactor favicon links in +layout.svelte and add session handling in sign_out_testing/+page.server.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,10 +27,16 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
 	<link rel="apple-touch-icon-precomposed" href="/favicons/apple-icon-precomposed.png" />
 	<link rel="apple-touch-icon" href="/favicons/apple-icon.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
 	<link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
-	<link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="icon" type="image/png" sizes="36x36" href="/favicons/android-icon-36x36.png" />
+    <link rel="icon" type="image/png" sizes="48x48" href="/favicons/android-icon-48x48.png" />
+    <link rel="icon" type="image/png" sizes="72x72" href="/favicons/android-icon-72x72.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/android-icon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="144x144" href="/favicons/android-icon-144x144.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
 	<link rel="manifest" href="/favicons/manifest.json" />
 	<meta name="msapplication-TileColor" content="#ffffff" />
 	<meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
--- src/routes/sign_out_testing/+page.server.ts
+++ src/routes/sign_out_testing/+page.server.ts
@@ -6,5 +6,21 @@ export const load: PageServerLoad = async () => {};
 
 export const actions: Actions = {
 	default: async (event) => {
+        console.log("event.locals: ", event.locals)
+		if (!event.locals.session) {
+			return fail(401);
+		}
+
+        console.log("event.locals: ", event.locals)
+        console.log("event.locals.session: ", event.locals.session)
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+
+        console.log("sessionCookie: ", sessionCookie)
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: ".",
+			...sessionCookie.attributes
+		});
+		redirect(302, "/");
 	}
 };
