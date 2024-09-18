251c2946997754a6bbb212b9a8e9ef7e356d6c14
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-18 23:08:38 +0200

    Refactor sign in page to improve authentication logic and session handling

--- src/routes/sign_in_testing/+page.server.ts
+++ src/routes/sign_in_testing/+page.server.ts
@@ -3,6 +3,7 @@ import { createPool } from '@vercel/postgres';
 import { POSTGRES_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { userTable, productsTable } from '$lib/drizzle/schema';
+import { eq } from 'drizzle-orm/expressions';
 import { lucia } from '$lib/server/auth';
 import { fail, redirect } from '@sveltejs/kit';
 import { verify } from '@node-rs/argon2';
@@ -13,6 +14,7 @@ export const load: PageServerLoad = async () => {};
 
 export const actions: Actions = {
 	default: async (event) => {
+        console.log("default")
 		const pool = createPool({ connectionString: POSTGRES_URL });
 		const db = drizzle(pool);
 
@@ -36,9 +38,47 @@ export const actions: Actions = {
 			});
 		}
 
-		const existingUser = await db
-			.select(userTable)
-			.where(userTable.username.equals(username))
-			.one();
+		console.log('username', username);
+		console.log('password', password);
+
+		const existingUsers = await db
+			.select()
+			.from(userTable)
+			.where(eq(userTable.username, username))
+			.limit(1);
+
+		const existingUser = existingUsers[0];
+
+		console.log('existingUser', existingUser);
+
+		if (!existingUser) {
+			return fail(400, {
+				message: 'Incorrect username or password'
+			});
+		}
+
+		const validPassword = await verify(existingUser.password_hash, password, {
+			memoryCost: 19456,
+			timeCost: 2,
+			outputLen: 32,
+			parallelism: 1
+		});
+
+		console.log('validPassword', validPassword);
+
+		if (!validPassword) {
+			return fail(400, {
+				message: 'Incorrect username or password'
+			});
+		}
+
+		const session = await lucia.createSession(existingUser.id, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+
+		redirect(302, '/');
 	}
 };
