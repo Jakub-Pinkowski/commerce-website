0103a9f49cd22db6a4c69bc970815cd72bc5d553
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-23 20:57:41 +0200

    Refactor register page form submission logic and add logout functionality

---
+++ src/routes/profile/logout/+page.server.ts
@@ -0,0 +1,22 @@
+import { lucia } from '$lib/server/auth';
+import { fail, redirect } from '@sveltejs/kit';
+
+import type { Actions, PageServerLoad } from './$types';
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+            console.log("event.locals.session is undefined");
+			return fail(401);
+		}
+		await lucia.invalidateSession(event.locals.session.id);
+		const sessionCookie = lucia.createBlankSessionCookie();
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
+        console.log("User logged out, redirecting to /profile/login");
+
+		redirect(302, '/profile/login');
+	}
+};
