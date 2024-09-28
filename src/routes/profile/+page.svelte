ad497db85900fd7a59a7e84a5aade913183db21a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-28 19:37:13 +0200

    Refactor profile page to update button styles, add "Your points" link, and improve code formatting

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -123,13 +123,52 @@
 				</ul>
 			</div>
 			<!-- TODO: Make it dynamic later -->
-			<div class="flex flex-col">
-				<ul>
+			<div class="flex w-full justify-between">
+				<ul class="flex-none">
 					<li>26.09.2024</li>
 					<li>Order number: 123456</li>
 					<li>Items: 3</li>
 					<li>Total: $150</li>
 				</ul>
+				<div class="flex max-h-40 max-w-2xl flex-grow justify-end gap-2">
+					<a
+						href="http://localhost:5173/products/hiking-backpack-20l"
+						class="max-w-40 flex-grow overflow-hidden rounded-lg border border-gray-300"
+					>
+						<img
+							src="https://jfj98xnxolpszjbj.public.blob.vercel-storage.com/assets/products/backpacks/hiking-backpack-20l/hiking-backpack-20l_0-6RdYmsQKes9PXdFe3g5bQeppap94Jx.webp"
+							alt=""
+						/>
+					</a>
+					<a
+						href="http://localhost:5173/products/lavender-dream-sneakers"
+						class="max-w-40 flex-grow overflow-hidden rounded-lg border border-gray-300"
+					>
+						<img
+							src="https://jfj98xnxolpszjbj.public.blob.vercel-storage.com/assets/products/shoes/lavender-dream-sneakers/lavender-dream-sneakers_0-yABKYSc3MQtDmiNrLnSqBAAe7byDzX.webp"
+							alt=""
+						/>
+					</a>
+					<a
+						href="http://localhost:5173/products/hiking-backpack-20l"
+						class="max-w-40 flex-grow overflow-hidden rounded-lg border border-gray-300"
+					>
+						<img
+							src="https://jfj98xnxolpszjbj.public.blob.vercel-storage.com/assets/products/backpacks/hiking-backpack-20l/hiking-backpack-20l_0-6RdYmsQKes9PXdFe3g5bQeppap94Jx.webp"
+							alt=""
+							class="h-full w-full object-cover"
+						/>
+					</a>
+					<a
+						href="http://localhost:5173/products/lavender-dream-sneakers"
+						class="max-w-40 flex-grow overflow-hidden rounded-lg border border-gray-300"
+					>
+						<img
+							src="https://jfj98xnxolpszjbj.public.blob.vercel-storage.com/assets/products/shoes/lavender-dream-sneakers/lavender-dream-sneakers_0-yABKYSc3MQtDmiNrLnSqBAAe7byDzX.webp"
+							alt=""
+						/>
+					</a>
+				</div>
 			</div>
 
 			<div class="card-actions mt-auto justify-end pt-2">
