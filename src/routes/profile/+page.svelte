8fc2923f27dee8833062613910b9bc76418e2ae1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-28 13:49:18 +0200

    Refactor profile page to add breadcrumbs and update navigation menu

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-	import UnderConstruction from '$lib/components/Common/UnderConstruction.svelte';
 	import { enhance } from '$app/forms';
+
+	import Breadcrumbs from '$lib/components/Common/Breadcrumbs.svelte';
+
+	let breadcrumbs = ['Home', 'Profile'];
 </script>
 
+<Breadcrumbs {breadcrumbs} />
 <div>
-	<UnderConstruction />
-	<form method="post" use:enhance>
-		<button class="btn btn-accent">Sign out</button>
-	</form>
+	<div>
+		<ul class="menu w-56 rounded-box bg-base-200">
+			<li class="menu-title">Your account</li>
+			<li><a href="/profile">Dashboard</a></li>
+			<li><a href="/profile/settings">Settings</a></li>
+			<li><a href="/profile/orders">Your orders</a></li>
+			<li><a href="/support">Help and FAQ</a></li>
+			<li>
+				<form method="post" use:enhance>
+					<button>Sign out</button>
+				</form>
+			</li>
+		</ul>
+	</div>
 </div>
---
+++ src/routes/profile/settings/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import UnderConstruction from '$lib/components/Common/UnderConstruction.svelte';
+</script>
+
+<div>
+	<UnderConstruction />
+</div>
