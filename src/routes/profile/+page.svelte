0ee3adbbbb1a89b0709678f0354a13b5b9772a9b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-28 14:04:20 +0200

    Refactor profile page to update navigation menu and improve code formatting

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -11,8 +11,8 @@
 	<ul class="menu w-56 rounded-box bg-base-200">
 		<li class="menu-title">Your account</li>
 		<li><a href="/profile">Dashboard</a></li>
-		<li><a href="/profile/settings">Settings</a></li>
 		<li><a href="/profile/orders">Your orders</a></li>
+		<li><a href="/profile/settings">Settings</a></li>
 		<li><a href="/support">Help and FAQ</a></li>
 		<li>
 			<form method="post" use:enhance>
@@ -20,7 +20,33 @@
 			</form>
 		</li>
 	</ul>
-	<div class="flex-grow">
-        
-    </div>
+	<div class="grid flex-grow grid-cols-2 gap-4">
+		<div class="card w-full bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Card title!</h2>
+				<p>If a dog chews shoes whose shoes does he choose?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+		<div class="card w-full bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Card title!</h2>
+				<p>If a dog chews shoes whose shoes does he choose?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+		<div class="card col-span-2 w-full bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Card title!</h2>
+				<p>If a dog chews shoes whose shoes does he choose?</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
