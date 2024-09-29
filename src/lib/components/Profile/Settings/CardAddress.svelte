91ee0f8d497d708beb9463b0cc1df83a415d86c7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-29 16:38:03 +0200

    Refactor CardAddress component to include consistent error handling, form submission, field validation, and toast/alert for successful address change

--- src/lib/components/Profile/Settings/CardAddress.svelte
+++ src/lib/components/Profile/Settings/CardAddress.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { quadOut } from 'svelte/easing';
+
 	import FormInput from '$lib/components/Common/FormInput.svelte';
 
 	import type { User } from '$lib/types/userTypes';
@@ -132,7 +135,7 @@
 			const message = data[1];
 			serverError = message;
 		} else if (result.type === 'success') {
-            // TODO: Show toast/alert when address was succesfully changed
+			// TODO: Show toast/alert when address was succesfully changed
 			toggleEdit();
 		}
 	};
@@ -208,6 +211,30 @@
 					{/each}
 				</tbody>
 			</table>
+			{#if serverError}
+				<div
+					role="alert"
+					class="alert alert-warning mt-4 text-whiteish"
+					transition:fade={{ delay: 0, duration: 200, easing: quadOut }}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+						/>
+					</svg>
+					<span>
+						{serverError}
+					</span>
+				</div>
+			{/if}
 		</div>
 		<div class="card-actions mt-auto justify-end pt-2">
 			{#if isEditing}
