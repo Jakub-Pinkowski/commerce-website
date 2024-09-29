e2a913e9f8bcba2a603fb356d258c2f198e18175
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-29 13:32:09 +0200

    Refactor profile settings page to include user data and improve code structure

--- src/routes/profile/settings/+page.svelte
+++ src/routes/profile/settings/+page.svelte
@@ -41,14 +41,6 @@
 									<td class="text-gray-400">No phone</td>
 								{/if}
 							</tr>
-							<tr>
-								<th>Address</th>
-								{#if user.address}
-									<td>{user.address}</td>
-								{:else}
-									<td class="text-gray-400">No address</td>
-								{/if}
-							</tr>
 						</tbody>
 					</table>
 				</div>
@@ -102,6 +94,29 @@
 									<td>{user.address.country}</td>
 								{:else}
 									<td class="text-gray-400">No country</td>
+								{/if}
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="card-actions mt-auto justify-end pt-2">
+					<a href="profile/settings" class="btn btn-accent w-full md:w-auto">Edit your info</a>
+				</div>
+			</div>
+		</div>
+		<div class="card flex w-full bg-base-100 shadow-xl">
+			<div class="card-body p-4 md:p-8">
+				<h2 class="card-title">Password</h2>
+				<!-- TODO: Style later -->
+				<div class="flex-none overflow-x-auto">
+					<table class="table">
+						<tbody>
+							<tr>
+								<th>Street</th>
+								{#if user.address?.street}
+									<td>{user.address.street}</td>
+								{:else}
+									<td class="text-gray-400">No steet</td>
 								{/if}
 							</tr>
 						</tbody>
