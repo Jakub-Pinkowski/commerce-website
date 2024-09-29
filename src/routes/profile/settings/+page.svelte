2e19fcfc72162c9bc4a3a7afe5403e1d3e304c6b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-09-29 12:37:39 +0200

    Refactor profile settings page to include user data and improve code structure

--- src/routes/profile/settings/+page.svelte
+++ src/routes/profile/settings/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import type { User } from '$lib/types/userTypes';
 
+	export let data;
+	const user: User = data.user;
+
+	console.log('user: ', user);
 </script>
 
 <div>
-
+	<h1 class="p-2 text-3xl font-bold">Settings</h1>
 </div>
