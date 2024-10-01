c9cf2091673155afeb3fe20b04199be1382368ae
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-01 19:38:54 +0200

    Refactor schema to include orders and order items tables

---
+++ src/lib/types/ordersTypes.ts
@@ -0,0 +1,15 @@
+export type Order = {
+    id: number;
+    user_id: string;
+    total_price: number;
+    status: string;
+    created_at: Date;
+};
+
+export type OrderItem = {
+    id: number;
+    order_id: number;
+    product_id: number;
+    quantity: number;
+    price: number;
+};
