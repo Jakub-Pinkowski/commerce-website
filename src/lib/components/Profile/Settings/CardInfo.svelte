e660928624d5bc79729e4385da914c6b076f0fdd
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-01 18:38:20 +0200

    Refactor CardInfo component to include form validation and submission

--- src/lib/components/Profile/Settings/CardInfo.svelte
+++ src/lib/components/Profile/Settings/CardInfo.svelte
@@ -2,6 +2,101 @@
 	import type { User } from '$lib/types/userTypes';
 
 	export let user: User;
+
+	let name: string;
+	let email: string;
+	let phone: string;
+	let nameError: string;
+	let emailError: string;
+	let phoneError: string;
+	let serverError: string;
+	let smallErrors: boolean = true;
+
+	const handleSubmit = async (event: Event) => {
+		event.preventDefault();
+
+		resetErrors();
+		validateFields();
+
+		if (nameError || emailError || phoneError) {
+			return;
+		}
+
+		const formData = new FormData();
+		if (user.id) {
+			formData.append('userId', user.id);
+		} else {
+			console.error('User ID is undefined');
+			return;
+		}
+
+		formData.append('name', name);
+		formData.append('email', email);
+		formData.append('phone', phone);
+
+		const response = await fetch('?/changeInfo', {
+			method: 'POST',
+			body: formData
+		});
+
+		const result = await response.json();
+		console.log('result: ', result);
+
+		if (result.type === 'failure') {
+			// NOTE: This is extremely wonky, hopefully when Svelte 5 releases it's going to be fixes
+			const data = JSON.parse(result.data);
+			const message = data[1];
+			serverError = message;
+		} else if (result.type === 'success') {
+			// TODO: Show toast/alert when address was succesfully changed
+			// TODO: Ideally I don't reload but pull the new instance of an user instead
+			location.reload();
+		}
+	};
+
+	const resetErrors = () => {
+		nameError = '';
+		emailError = '';
+		phoneError = '';
+		serverError = '';
+	};
+
+	const validateFields = () => {
+		if (!name) {
+			nameError = 'Name is required';
+		}
+
+		if (!email) {
+			emailError = 'Email is required';
+		}
+
+		if (!phone) {
+			phoneError = 'Phone is required';
+		}
+
+		if (email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+			emailError = 'Invalid email';
+		}
+
+		if (phone && !/^\d{10}$/.test(phone)) {
+			phoneError = 'Invalid phone number';
+		}
+	};
+
+	const handleNameInteraction = () => {
+		nameError = '';
+		serverError = '';
+	};
+
+	const handleEmailInteraction = () => {
+		emailError = '';
+		serverError = '';
+	};
+
+	const handlePhoneInteraction = () => {
+		phoneError = '';
+		serverError = '';
+	};
 </script>
 
 <div class="card flex w-full bg-base-100 shadow-xl">
