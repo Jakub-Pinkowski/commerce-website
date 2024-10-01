46dc9db4ce18787d2db7780b8b32b4add795a8a0
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-01 19:46:20 +0200

    Refactor profile/orders page to include multiple card orders

--- src/routes/profile/orders/+page.svelte
+++ src/routes/profile/orders/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-	import type { User } from '$lib/types/userTypes';
-    
 	import CardOrders from '$lib/components/Profile/Dashboard/CardOrders.svelte';
+
+	import type { User } from '$lib/types/userTypes';
+
+	export let data;
+	const user: User = data.user;
 </script>
 
 <h1 class="p-2 text-3xl font-bold">Orders</h1>
 
-<div class="grid flex-grow grid-cols-1 gap-4"></div>
+<div class="grid flex-grow grid-cols-1 gap-4">
+	<CardOrders {user} />
+	<CardOrders {user} />
+</div>
