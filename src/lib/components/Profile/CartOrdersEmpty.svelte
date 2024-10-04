fb4c903957cf22a77b365f388ef5715c11841fb9
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-04 23:26:04 +0200

    Refactor CartOrdersEmpty.svelte component to display a card with a message when there are no orders

--- src/lib/components/Profile/CartOrdersEmpty.svelte
+++ src/lib/components/Profile/CartOrdersEmpty.svelte
@@ -1,1 +1,5 @@
-<div>No orders</div>
+<div class="card w-full bg-base-100 shadow-xl md:col-span-2">
+	<div class="card-body p-4 md:p-8">
+		<p>You haven't placed any orders yet. Once you do, you'll be able to see them here.</p>
+	</div>
+</div>
