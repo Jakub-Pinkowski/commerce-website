86795465fcdde940f0566e9b21c82f7f18eac1cf
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-04 18:14:15 +0200

    Refactor CardOrders.svelte to improve order details display and update field names

--- src/lib/components/Profile/CardOrders.svelte
+++ src/lib/components/Profile/CardOrders.svelte
@@ -322,6 +322,33 @@
 			<!-- TODO: Change the look of this button -->
 			<button class="btn btn-primary w-full md:w-auto" on:click={toggleExpand}>
 				{isExpanded ? 'Hide details' : 'See more details'}
+				{#if isExpanded}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						id="Outline"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+						fill="#eaeaea"
+					>
+						<path
+							d="M18,15.5a1,1,0,0,1-.71-.29l-4.58-4.59a1,1,0,0,0-1.42,0L6.71,15.21a1,1,0,0,1-1.42-1.42L9.88,9.21a3.06,3.06,0,0,1,4.24,0l4.59,4.58a1,1,0,0,1,0,1.42A1,1,0,0,1,18,15.5Z"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						id="Outline"
+						viewBox="0 0 24 24"
+						width="24"
+						height="24"
+						fill="#eaeaea"
+					>
+						<path
+							d="M18.71,8.21a1,1,0,0,0-1.42,0l-4.58,4.58a1,1,0,0,1-1.42,0L6.71,8.21a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.59,4.59a3,3,0,0,0,4.24,0l4.59-4.59A1,1,0,0,0,18.71,8.21Z"
+						/>
+					</svg>
+				{/if}
 			</button>
 			{#if recentOrder}
 				<a href="profile/orders" class="btn btn-accent w-full md:w-auto">See all orders</a>
