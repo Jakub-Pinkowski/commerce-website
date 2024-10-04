10b1106a424929f1a12c3b4722cfb1d7699967a1
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-04 22:10:47 +0200

    Refactor CardPointsHistory.svelte to include responsive table layout

--- src/lib/components/Profile/Points/CardPointsHistory.svelte
+++ src/lib/components/Profile/Points/CardPointsHistory.svelte
@@ -21,7 +21,8 @@
 	<div class="card-body p-4 md:p-8">
 		<h2 class="card-title">Your points history</h2>
 		<p>Points are added to your account 30 days after the order has been completed.</p>
-		<table class="table">
+		<!-- Desktop -->
+		<table class="table hidden md:table">
 			<tbody>
 				<tr>
 					<th>Date</th>
@@ -39,5 +40,32 @@
 				{/each}
 			</tbody>
 		</table>
+		<!-- Mobile -->
+		<table class="table md:hidden">
+			<tbody>
+				<tr>
+					<th class="!pl-4">Date</th>
+					<th>Order</th>
+					<th>Status</th>
+					<th>Points</th>
+				</tr>
+				{#each orders as order}
+					<tr>
+						<td>{formatDate(order.createdAt)}</td>
+						<td>{order.id}</td>
+						<td>{getPointsStatus(order.createdAt)}</td>
+						<td class="font-bold text-primary">{order.points}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
+
+<style scoped>
+	@media (max-width: 768px) {
+		.table :where(th, td) {
+			padding-right: 1rem !important;
+		}
+	}
+</style>
