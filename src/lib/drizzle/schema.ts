4e5ac14d3f1d977b664a8eaf7dcf4896d5f796fb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-04 13:41:26 +0200

    Refactor CardOrders.svelte to update order rendering and improve UI

--- src/lib/drizzle/schema.ts
+++ src/lib/drizzle/schema.ts
@@ -27,7 +27,7 @@ export const productsTable = pgTable('products', {
 	url: varchar('url', { length: 255 }).notNull(),
 	imageurl: varchar('imageurl', { length: 255 }).notNull(),
 	alternate_images: text('alternate_images').array().notNull(),
-    created_at: timestamp('created_at').defaultNow().notNull()
+	created_at: timestamp('created_at').defaultNow().notNull()
 });
 
 // TODO: Add better types later on varchar etc.
@@ -72,8 +72,25 @@ export const ordersTable = pgTable('orders', {
 		.notNull()
 		.references(() => usersTable.id),
 	total_price: decimal('total_price').notNull(),
-	status: varchar('status').notNull(),
-    created_at: timestamp('created_at').defaultNow().notNull()
+	status: varchar('status').notNull(), // CONSTRAINS: 'placed', 'processed', 'dispatched', 'delivered', 'returned', 'cancelled'
+	created_at: timestamp('created_at').defaultNow().notNull(),
+	updated_at: timestamp('updated_at').defaultNow().notNull(),
+	delivery_address_street: varchar('delivery_address_street').notNull(),
+	delivery_address_city: varchar('delivery_address_city').notNull(),
+	delivery_address_state: varchar('delivery_address_state').notNull(),
+	delivery_address_postalcode: varchar('delivery_address_postalcode').notNull(),
+	delivery_address_country: varchar('delivery_address_country').notNull(),
+	invoice_address_street: varchar('invoice_address_street').notNull(),
+	invoice_address_city: varchar('invoice_address_city').notNull(),
+	invoice_address_state: varchar('invoice_address_state').notNull(),
+	invoice_address_postalcode: varchar('invoice_address_postalcode').notNull(),
+	invoice_address_country: varchar('invoice_address_country').notNull(),
+	payment_method: varchar('payment_method').notNull(), // CONSTRAINS: 'debit_card, 'credit_card', 'paypal', 'cash_on_delivery'
+	payment_status: varchar('payment_status').notNull(), // CONSTRAINS: 'pending', 'completed', 'failed'
+	shipping_method: varchar('shipping_method').notNull(), // CONSTRAINS: 'standard', 'express', 'overnight'
+	tracking_number: varchar('tracking_number'),
+	points: integer('points').notNull(),
+	customer_notes: text('customer_notes')
 });
 
 export const orderItemsTable = pgTable('order_items', {
