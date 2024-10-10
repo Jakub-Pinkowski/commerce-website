f3822891f4525d83b975f55d0c3f2e281419bbf3
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-10 22:40:26 +0200

    Refactor code: Update database query in product page server file

--- src/routes/products/[slug]/+page.server.ts
+++ src/routes/products/[slug]/+page.server.ts
@@ -5,12 +5,9 @@ import { db, mapProducts } from '$lib/helpers/drizzle';
 
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ params }) => {
-	const productQuery = mapProducts(
-		await db.select().from(productsTable).where(eq(productsTable.handle, params.slug))
-	);
-	const product = productQuery[0];
-
-	console.log('product', product);
+	const product = mapProducts(
+		await db.select().from(productsTable).limit(1).where(eq(productsTable.handle, params.slug))
+	)[0];
 
 	return {
 		product
