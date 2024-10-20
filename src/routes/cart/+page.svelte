8f5ae10cc8b7bd6c400cb66a90ed1a945af5cf38
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-20 22:37:43 +0200

    Refactor code: Update MiniCart component to display total list price if it's higher than the total price

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -55,6 +55,27 @@
 						</span>
 					</div>
 				</div>
+				{#if totalListPrice > total}
+					<div role="alert" class="alert alert-add-to-cart">
+						<!-- TODO: Change this svg to some dollar icon -->
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6 shrink-0 stroke-current"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<span class="ml-auto">
+							You are saving ${totalListPrice - total}!
+						</span>
+					</div>
+				{/if}
 				<div class="flex justify-end">
 					<a href="/cart/checkout" class="btn btn-primary">Proceed to checkout</a>
 				</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,11 @@ export default {
 				'secondary-accent': '#f4a261', // Orange
 				'dark-secondary-accent': '#e76f51', // Dark Orange
 				'main-red': '#e63946', // Red
-                'tier-bronze': '#cd7f32', // Tier - Bronze
-                'tier-silver': '#adb5bd', // Tier - Silver
-                'tier-gold': '#ffd700', // Tier - Gold
-                'tier-platinum': '#415a77', // Tier - Platinum
-                'tier-diamond': '#48cae4', // Tier - Diamond
+				'tier-bronze': '#cd7f32', // Tier - Bronze
+				'tier-silver': '#adb5bd', // Tier - Silver
+				'tier-gold': '#ffd700', // Tier - Gold
+				'tier-platinum': '#415a77', // Tier - Platinum
+				'tier-diamond': '#48cae4' // Tier - Diamond
 			}
 		}
 	},
