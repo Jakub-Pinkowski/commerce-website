0e32d933d6ffa74441d87ea30f91c2c3034dba51
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-10-20 16:25:41 +0200

    Refactor code: Update import path for CartProductCard component

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
-    // TODO: Should lead to checkout, include recommendation carousel
-	import UnderConstruction from '$lib/components/Common/UnderConstruction.svelte';
+	import { onMount } from 'svelte';
+	// TODO: Should lead to checkout, include recommendation carousel
+	import { cart } from '$lib/stores/cart';
+	import Breadcrumbs from '$lib/components/Common/Breadcrumbs.svelte';
+	import CartProductCard from '$lib/components/Header/CartProductCard.svelte';
+
+	import type { CartItem } from '$lib/stores/cart';
+
+	let breadcrumbs = ['Home', 'Cart'];
+	let items: CartItem[] = [];
+
+	onMount(() => {
+		const unsubscribeCart = cart.subscribe((value) => {
+			items = value;
+		});
+
+		return () => {
+			document.body.classList.remove('overflow-hidden');
+			unsubscribeCart();
+		};
+	});
+
+	$: total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
 </script>
 
 <div>
-	<UnderConstruction />
+	<Breadcrumbs {breadcrumbs} />
+
+	<h1 class="text-3xl font-bold">Cart</h1>
 </div>
