cc5a9658bc24b43cc5e4969f387b501fb24d5bca
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-26 15:33:18 +0100

    Add PromoSection component to Homepage
    The PromoSection component centralizes and replaces the previous experimental section for promotions. It ensures better maintainability by moving the promo logic and layout into a dedicated Svelte component. This improves code structure and reusability.

---
+++ src/lib/components/Homepage/PromoSection.svelte
@@ -0,0 +1,44 @@
+	<section class="my-8 flex justify-center">
+		<div class="stats stats-vertical md:stats-horizontal text-center">
+			<div class="stat w-full">
+				<div class="stat-value mb-4">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						id="Layer_1"
+						data-name="Layer 1"
+						viewBox="0 0 24 24"
+						width="36"
+						height="36"
+						class="inline-block"
+					>
+						<path
+							d="m24,10c0-2.757-2.243-5-5-5h-2v-1c0-1.654-1.346-3-3-3h-2v2h2c.552,0,1,.449,1,1v13H2v-4H0v6h2.037c-.024.165-.037.331-.037.5,0,1.93,1.57,3.5,3.5,3.5s3.5-1.57,3.5-3.5c0-.169-.013-.335-.037-.5h6.074c-.024.165-.037.331-.037.5,0,1.93,1.57,3.5,3.5,3.5s3.5-1.57,3.5-3.5c0-.169-.013-.335-.037-.5h2.037v-9ZM7,19.5c0,.827-.673,1.5-1.5,1.5s-1.5-.673-1.5-1.5c0-.189.037-.356.091-.5h2.819c.054.144.091.311.091.5Zm12-12.5c1.654,0,3,1.346,3,3v1h-5v-4h2Zm1,12.5c0,.827-.673,1.5-1.5,1.5s-1.5-.673-1.5-1.5c0-.189.037-.356.091-.5h2.819c.054.144.091.311.091.5Zm-3-2.5v-4h5v4h-5ZM10,3H0V1h10v2Zm-2,4H0v-2h8v2Zm-2,4H0v-2h6v2Z"
+						/>
+					</svg>
+				</div>
+				<div class="stat-title font-semibold whitespace-normal">
+					Free shipping for orders over €99
+				</div>
+			</div>
+			<div class="stat w-full">
+				<div class="stat-value mb-4">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						id="Layer_1"
+						data-name="Layer 1"
+						viewBox="0 0 24 24"
+						width="36"
+						height="36"
+						class="inline-block"
+					>
+						<path
+							d="m22,12h2c0,6.617-5.383,12-12,12-4.055,0-7.794-2.058-9.995-5.368l-.005,2.368H0v-3.991c0-1.107.901-2.009,2.008-2.009h3.992v2h-2.637c1.776,3.06,5.052,5,8.637,5,5.514,0,10-4.486,10-10Zm0-9l-.005,2.353C19.806,2.04,16.106,0,12,0,5.383,0,0,5.383,0,12h2C2,6.486,6.486,2,12,2c3.64,0,6.9,1.921,8.666,5h-2.666v2h3.991c1.107,0,2.009-.901,2.009-2.009v-3.991h-2Zm-5,14H7v-7.5c0-1.379,1.122-2.5,2.5-2.5h5c1.379,0,2.5,1.121,2.5,2.5v7.5Zm-2-7.5c0-.275-.225-.5-.5-.5h-5c-.276,0-.5.225-.5.5v5.5h6v-5.5Zm-4.5,2.5h3v-2h-3v2Z"
+						/>
+					</svg>
+				</div>
+				<div class="stat-title font-semibold whitespace-normal">
+					Free returns and exchanges within 30 days
+				</div>
+			</div>
+		</div>
+	</section>
